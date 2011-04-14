d1a20294ff3b2ec2b15cca84dacc8ab28ddaee6a
Michael Aufreiter <ma@zive.at>
2011-04-14 21:18:21 +0200

    Restructured views (Project, Visualization => Project, Sheet). Aggregation functions can now be chosen by the user.

--- public/javascripts/views/project.js
+++ public/javascripts/views/project.js
@@ -3,8 +3,6 @@
 
 var Project = Backbone.View.extend({
   events: {
-    'click .add-choice': 'addChoice',
-    'click .remove-choice': 'removeChoice',
     'click .new-sheet': 'newSheet',
     'click .sheet': 'switchSheet',
     'click a.delete-project': 'delete',
@@ -17,6 +15,8 @@ var Project = Backbone.View.extend({
   initialize: function(options) {
     this.app = options.app;
     _.bindAll(this, "render");
+    
+    this.sheet = new Sheet({project: this});
   },
   
   switchSheet: function() {
@@ -42,71 +42,6 @@ var Project = Backbone.View.extend({
       this.close();
     }
     return false;
-  },
-  
-  initSheet: function() {
-    var that = this;
-    
-    // Init visualization view
-    this.visualization = new Visualization({model: this.filteredCollection, project: this});
-    
-    // Init filters
-    this.filters = new Data.Hash();
-    this.settings = this.activeSheet.get('settings') || {};
-    
-    this.collection.properties().each(function(property, key) {
-      if (property.meta.facet) {
-        
-        that.filters.set(key, {
-          operator: '|=',
-          values: new Data.Hash(),
-          objects: new Data.Hash()
-        });
-        
-        // Import values from settings
-        if (that.settings && that.settings.filters) {
-          _.each(that.settings.filters[key].values, function(val) {
-            that.addValue(key, val);
-          });
-        }
-      }
-    });
-    
-    this.filter();
-    this.updateFacets();
-    
-
-  },
-  
-  loadSheet: function(sheet) {
-    var that = this;
-    
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      data: {
-        sheet: sheet._id
-      },
-      dataType: "json",
-      success: function(res) {
-        if (!res.status) {
-          that.collection = new Data.Collection(res);
-          that.previousCollection = that.collection;
-          that.filteredCollection = that.collection;
-          that.activeSheet = sheet;
-          that.initSheet()
-          that.render();
-          
-          // Make editable
-          that.makeEditable();
-        } else {
-          $('#project_wrapper').html("<div id=\"project\"><div id=\"project_header\"><h2>The sheet couldn't be loaded.</h2><p>You may not be permitted to access the datasource.<br/><br/></p></div></div>");
-        }
-      },
-      error: function(err) {
-        $('#project_wrapper').html("The sheet couldn't be loaded.");
-      }
-    });
   },
   
   makeEditable: function() {
@@ -140,10 +75,12 @@ var Project = Backbone.View.extend({
       that.model = graph.get(id);
 
       if (that.model) {
-        that.loadSheet(that.model.get('sheets').first());
+        // Load first sheet by default
+        that.sheet.load(that.model.get('sheets').first());
         
-        // that.trigger('changed');
-        // that.loadedProjects[username+"/"+projectname] = id;
+        that.sheet.bind('loaded', function() {
+          that.activeSheet = sheet;
+        });
         
         // Update browser graph reference
         app.browser.graph.set('objects', id, that.model);
@@ -161,8 +98,6 @@ var Project = Backbone.View.extend({
     
     $('#project_tab').show();
     $('#project_tab').html('&nbsp;&nbsp;&nbsp;Loading...');
-    
-    $('#project_wrapper').html("<div id=\"project\"><div id=\"project_header\"><h2>Loading project...</h2><p>Depending on the amount of data this may take a while.<br/><br/></p></div></div>");
     
     $.ajax({
       type: "GET",
@@ -220,14 +155,6 @@ var Project = Backbone.View.extend({
     });
   },
   
-  // Close Project
-  close: function() {
-    this.model = null;
-    controller.saveLocation('#'+app.username);
-    $('#project_tab').hide();
-    app.toggleView('browser');
-  },
-  
   renderTab: function() {
     if (this.model) {
       $('#project_tab').show();
@@ -238,207 +165,22 @@ var Project = Backbone.View.extend({
     }
   },
   
-  addChoice: function(e) {
-      var property = $(e.currentTarget).attr('property'),
-          operator = $(e.currentTarget).attr('operator'),
-          value = $(e.currentTarget).attr('value');
-    
-    this.addValue(property, value);
-    
-    this.filter(property);
-    
-    this.updateFacets();
-    this.render();
-    this.storeSettings();
-    return false;
+  // Close Project
+  close: function() {
+    this.model = null;
+    controller.saveLocation('#'+app.username);
+    $('#project_tab').hide();
+    app.toggleView('browser');
   },
-  
-  removeChoice: function(e) {
-    var property = $(e.currentTarget).attr('property'),
-        operator = $(e.currentTarget).attr('operator'),
-        value = $(e.currentTarget).attr('value');
-    
-    this.removeValue(property, value);
-    
-    this.filter(property);
-    
-    this.updateFacets();
-    this.render();
-    this.storeSettings();
-    return false;
-  },
-  
-  // Extract query from filters
-  query: function() {
-    var query = {};
-    this.filters.each(function(f, key) {
-      if (f.values.length > 0) query[key+f.operator] = f.values.values();
-    });
-    return query;
-  },
-  
-  // Update facet objects based on a new value
-  addValue: function(property, value) {
-    var p = this.collection.properties().get(property);
-    var filter = this.filters.get(property);
-    
-    // Skip values that do not exist in the current dataset
-    if (!p.all('values').get(value)) return;
-    // Add new value to the filter
-    filter.values.set(value, value);
-    filter.objects = filter.objects.union(p.all('values').get(value).referencedObjects);
-    
-    // this.filter(property);
-  },
-  
-  // Update facet objects by removing a particular value
-  removeValue: function(property, value) {
-    var p = this.collection.properties().get(property);
-    var filter = this.filters.get(property);
-    // Remove value from the filter
-    filter.values.del(value);
-    filter.objects = filter.objects.difference(p.all('values').get(value).referencedObjects);
-    
-    // this.filter(property);
-  },
-  
-  // Get values (=facet-choices) for a particular property
-  valuesForProperty: function(property) {
-    var activeItems,
-        values;
-    
-    this.filters.each(function(filter, key, index) {
-      if (key !== property && filter.values.length > 0) {
-        if (!activeItems) {
-          activeItems = filter.objects;
-        } else {
-          activeItems = activeItems.intersect(filter.objects);
-        }
-      }
-    });
-    
-    if (!activeItems) {
-      values = this.collection.properties().get(property).all('values');
-    } else {
-      // Construct a collection and use it for value extraction
-      var props = {};
-      props[property] = this.collection.properties().get(property).toJSON();
-      var c  = new Data.Collection({
-        items: activeItems.toJSON(),
-        properties: props
-      });
-      values = c.properties().get(property).all('values')
-    }
-    
-    // Sort values
-    var DESC_BY_OBJECT_COUNT = function(item1, item2) {
-      var v1 = item1.value.referencedObjects.length,
-          v2 = item2.value.referencedObjects.length;
-      return v1 === v2 ? 0 : (v1 > v2 ? -1 : 1);
-    };
-    return values.sort(DESC_BY_OBJECT_COUNT);
-  },
-  
-  // Perform filters on the input collection
-  filter: function(activeProperty) {
-    var cspec = {};
-    
-    // TODO: use the smallest first in order to optimize intersection()
-    var items;
-    
-    this.filters.each(function(filter, key, index) {
-      if (filter.values.length > 0) {
-        if (!items) {
-          items = filter.objects;
-        } else {
-          items = items.intersect(filter.objects);
-        }
-      }
-    });
-    
-    if (!items) { // no filters at all
-      this.filteredCollection = this.collection;
-      this.activeFacetCollection = this.collection;
-    } else {
-      this.filteredCollection = new Data.Collection({
-        items: items.toJSON(),
-        properties: this.collection.properties().toJSON()
-      });
-    }
-    
-    // Update visualization
-    this.visualization.update(this.filteredCollection, this.settings.group_key, this.settings.properties);
-  },
-  
-  importSettings: function(settings) {
-    
-  },
-  
-  storeSettings: function() {
-    var that = this;
-    // Settings are only stored for the owner
-    if (this.model.get('creator')._id !== "/user/"+app.username) return;
-    
-    this.settings = {filters: {}, group_key: null, properties: []};
-    this.filters.each(function(filter, key) {
-      that.settings.filters[key] = {
-        operator: "|=",
-        values: filter.values.keys()
-      }
-    });
-    
-    this.settings.group_key = this.visualization.groupKey;
-    this.settings.properties = this.visualization.properties.keys();
-    
-    this.activeSheet.set({
-      settings: that.settings
-    });
-  },
-  
-  // Facets and current Facet choices in order to power the view
-  updateFacets: function() {
-    var facets = new Data.Hash();
-    var that = this;
-    
-    this.collection.properties().each(function(property, key) {
-      if (property.meta.facet) {
-        var choices = new Data.Hash();
-        var selectedChoices = new Data.Hash();
-        var p = that.collection.properties().get(key);
-        
-        that.valuesForProperty(key).each(function(value, valueKey) {
-          if (that.filters.get(key) && that.filters.get(key).values.get(valueKey)) {
-            selectedChoices.set(valueKey, {_id: valueKey, name: valueKey.slice(0, 26), count: value.referencedObjects.length});
-          } else {
-            choices.set(valueKey, {_id: valueKey, name: valueKey.slice(0, 26), count: value.referencedObjects.length});
-          }
-        });
-        
-        facets.set(key, {
-          property: p,
-          selected_choices: selectedChoices,
-          choices: choices
-        });
-      }
-    });
-    this.facets = facets;
-  },
-  
+
   render: function() {
-    // If sheet loaded
-    if (this.activeSheet) {
-      $(this.el).html(_.tpl('project', {
-        project: this.model,
-        facets: this.facets,
-        collection: this.collection,
-        filtered_collection: this.filteredCollection
-      }));
-      
-      this.renderTab();
-      this.visualization.render();
-    } else {
-      $('#project_tab').html('&nbsp;&nbsp;&nbsp;Loading data...');
-    }
+    $(this.el).html(_.tpl('project', {
+      project: this.model,
+    }));
+    
+    this.renderTab();
+    this.sheet.render();
+    
     this.delegateEvents();
     return this;
   }
---
+++ public/javascripts/views/sheet.js
@@ -0,0 +1,389 @@
+// Visualization
+// --------------
+
+// Map property keys to colors
+_.propertyColors = function(properties) {
+  return d3.scale.ordinal().domain(properties).range(['#8DB5C8', '#90963C', '#B16649', '#A2C355', '#93BAA1', '#808E89', '#86A2A9']);
+};
+
+var Sheet = Backbone.View.extend({
+  events: {
+    'change #group_key': 'updateGroupKey',
+    'click .add-choice': 'addChoice',
+    'click .remove-choice': 'removeChoice',
+    'change #group_key': 'updateGroupKey',
+    'click .property.select': 'selectProperty',
+    'click .property.deselect': 'deselectProperty',
+    'change select.aggregator': 'switchAggregator'
+  },
+  
+  el: '#sheet',
+  
+  initialize: function(options) {
+    // Default properties
+    this.project = options.project;
+    this.groupKey = [];
+    this.properties = [];
+    this.settings = {};
+    
+    // Initialize visualization instance
+    this.visualization = new Barchart('#canvas', {});
+  },
+  
+  switchAggregator: function(e) {
+    var propertyIndex = $(e.currentTarget).parent().attr('index'),
+        aggregator = $(e.currentTarget).val();
+    
+    this.properties[propertyIndex].aggregator = aggregator;
+    this.compute();
+    this.render();
+    this.storeSettings();
+  },
+  
+  initSheet: function() {
+    var that = this;
+    
+    // Init filters
+    this.filters = new Data.Hash();
+    this.settings = this.model.get('settings') || {};
+    
+    if (this.settings.properties && this.settings.properties.length > 0 && typeof this.settings.properties[0] == 'object') {
+      this.properties = this.settings.properties;
+      this.groupKey = this.settings.group_key;
+    } else {
+      // Init properties
+      this.availableProperties().each(function(p, key) {
+        that.properties.push({
+          property: key,
+          key: Data.uuid(),
+          name: p.name,
+          selected: false,
+          aggregator: "SUM"
+        });
+      });
+      
+      // Default groupKey
+      this.groupKey = [this.groupKeys()[0].key];
+    }
+    
+    this.collection.properties().each(function(property, key) {
+      if (property.meta.facet) {
+        that.filters.set(key, {
+          operator: '|=',
+          values: new Data.Hash(),
+          objects: new Data.Hash()
+        });
+        
+        // Import values from settings
+        if (that.settings && that.settings.filters) {
+          if (!that.settings.filters[key]) return;
+          _.each(that.settings.filters[key].values, function(val) {
+            that.addValue(key, val);
+          });
+        }
+      }
+    });
+    
+    this.filter();
+    this.updateFacets();
+  },
+  
+  load: function(sheet) {
+    var that = this;
+    that.collection = that.filteredCollection = null;
+    
+    that.model = sheet;
+    $.ajax({
+      type: "GET",
+      url: "/data",
+      data: {
+        sheet: sheet._id
+      },
+      dataType: "json",
+      success: function(res) {
+        if (!res.status) {
+          that.collection = new Data.Collection(res);
+          that.filteredCollection = that.collection;
+          that.initSheet();
+          that.trigger('loaded');
+          that.compute();
+          that.render();
+        } else {
+          $('#project_wrapper').html("<div id=\"project\"><div id=\"project_header\"><h2>The sheet couldn't be loaded.</h2><p>You may not be permitted to access the datasource.<br/><br/></p></div></div>");
+        }
+      },
+      error: function(err) {
+        $('#project_wrapper').html("The sheet couldn't be loaded.");
+      }
+    });
+  },
+  
+  propertyColors: function() {
+    return _.propertyColors(this.properties.map(function(p) { return p.property }));
+  },
+  
+  addChoice: function(e) {
+      var property = $(e.currentTarget).attr('property'),
+          operator = $(e.currentTarget).attr('operator'),
+          value = $(e.currentTarget).attr('value');
+    
+    this.addValue(property, value);
+    this.filter(property);
+    this.updateFacets();
+    this.render();
+    this.storeSettings();
+    return false;
+  },
+  
+  removeChoice: function(e) {
+    var property = $(e.currentTarget).attr('property'),
+        operator = $(e.currentTarget).attr('operator'),
+        value = $(e.currentTarget).attr('value');
+    
+    this.removeValue(property, value);
+    
+    this.filter(property);
+    this.updateFacets();
+    this.render();
+    this.storeSettings();
+    return false;
+  },
+  
+  // Extract query from filters
+  query: function() {
+    var query = {};
+    this.filters.each(function(f, key) {
+      if (f.values.length > 0) query[key+f.operator] = f.values.values();
+    });
+    return query;
+  },
+  
+  // Update facet objects based on a new value
+  addValue: function(property, value) {
+    var p = this.collection.properties().get(property);
+    var filter = this.filters.get(property);
+    
+    // Skip values that do not exist in the current dataset
+    if (!p.all('values').get(value)) return;
+    // Add new value to the filter
+    filter.values.set(value, value);
+    filter.objects = filter.objects.union(p.all('values').get(value).referencedObjects);
+    
+    // this.filter(property);
+  },
+  
+  // Update facet objects by removing a particular value
+  removeValue: function(property, value) {
+    var p = this.collection.properties().get(property);
+    var filter = this.filters.get(property);
+    
+    // Remove value from the filter
+    filter.values.del(value);
+    filter.objects = filter.objects.difference(p.all('values').get(value).referencedObjects);
+    
+    // this.filter(property);
+  },
+  
+  // Get values (=facet-choices) for a particular property
+  valuesForProperty: function(property) {
+    var activeItems,
+        values;
+    
+    this.filters.each(function(filter, key, index) {
+      if (key !== property && filter.values.length > 0) {
+        if (!activeItems) {
+          activeItems = filter.objects;
+        } else {
+          activeItems = activeItems.intersect(filter.objects);
+        }
+      }
+    });
+    
+    if (!activeItems) {
+      values = this.collection.properties().get(property).all('values');
+    } else {
+      // Construct a collection and use it for value extraction
+      var props = {};
+      props[property] = this.collection.properties().get(property).toJSON();
+      var c  = new Data.Collection({
+        items: activeItems.toJSON(),
+        properties: props
+      });
+      values = c.properties().get(property).all('values')
+    }
+    
+    // Sort values
+    var DESC_BY_OBJECT_COUNT = function(item1, item2) {
+      var v1 = item1.value.referencedObjects.length,
+          v2 = item2.value.referencedObjects.length;
+      return v1 === v2 ? 0 : (v1 > v2 ? -1 : 1);
+    };
+    return values.sort(DESC_BY_OBJECT_COUNT);
+  },
+  
+  // Perform filters on the input collection
+  filter: function(activeProperty) {
+    var cspec = {};
+    
+    // TODO: use the smallest first in order to optimize intersection()
+    var items;
+    
+    this.filters.each(function(filter, key, index) {
+      if (filter.values.length > 0) {
+        if (!items) {
+          items = filter.objects;
+        } else {
+          items = items.intersect(filter.objects);
+        }
+      }
+    });
+    
+    if (!items) { // no filters at all
+      this.filteredCollection = this.collection;
+      this.activeFacetCollection = this.collection;
+    } else {
+      this.filteredCollection = new Data.Collection({
+        items: items.toJSON(),
+        properties: this.collection.properties().toJSON()
+      });
+    }
+  },
+  
+  storeSettings: function() {
+    var that = this;
+    // Settings are only stored for the owner
+    if (this.project.model.get('creator')._id !== "/user/"+app.username) return;
+    
+    this.settings = {filters: {}, group_key: null, properties: []};
+    this.filters.each(function(filter, key) {
+      that.settings.filters[key] = {
+        operator: "|=",
+        values: filter.values.keys()
+      }
+    });
+    
+    this.settings.group_key = this.groupKey;
+    this.settings.properties = this.properties;
+    
+    this.model.set({
+      settings: that.settings
+    });
+  },
+  
+  // Facets and current Facet choices in order to power the view
+  updateFacets: function() {
+    var facets = new Data.Hash();
+    var that = this;
+    
+    this.collection.properties().each(function(property, key) {
+      if (property.meta.facet) {
+        var choices = new Data.Hash();
+        var selectedChoices = new Data.Hash();
+        var p = that.collection.properties().get(key);
+        
+        that.valuesForProperty(key).each(function(value, valueKey) {
+          if (that.filters.get(key) && that.filters.get(key).values.get(valueKey)) {
+            selectedChoices.set(valueKey, {_id: valueKey, name: valueKey.slice(0, 26), count: value.referencedObjects.length});
+          } else {
+            choices.set(valueKey, {_id: valueKey, name: valueKey.slice(0, 26), count: value.referencedObjects.length});
+          }
+        });
+        
+        facets.set(key, {
+          property: p,
+          selected_choices: selectedChoices,
+          choices: choices
+        });
+      }
+    });
+    this.facets = facets;
+  },
+  
+  
+  updateGroupKey: function() {
+    this.groupKey = [$('#group_key').val()];
+    this.compute();
+    this.render();
+    this.storeSettings();
+    return false;
+  },
+  
+  selectProperty: function(e) {
+    this.properties[$(e.currentTarget).attr('index')].selected = true;
+    this.compute();
+    this.render();
+    this.storeSettings();
+    return false;
+  },
+  
+  deselectProperty: function(e) {
+    this.properties[$(e.currentTarget).attr('index')].selected = false;
+    this.compute();
+    this.render();
+    this.storeSettings();
+    return false;
+  },
+  
+  availableProperties: function() {
+    return this.collection.properties().select(function(p) {
+      return p.expectedTypes[0] === 'number' && p.unique === true
+    });
+  },
+  
+  // Extract group keys
+  groupKeys: function() {
+    return this.collection.properties().select(function(p) {
+      return p.expectedTypes[0] === 'string'
+    });
+  },
+  
+  compute: function() {
+    var properties = {};
+    
+    _.each(this.properties, function(p, index) {
+      if (!p.selected) return;
+      properties[p.property] = {aggregator: Data.Aggregators[p.aggregator]};
+    });
+    
+    if (this.groupKey.length > 0) {
+      this.groupedItems = this.collection.group(this.groupKey, properties);
+    } else {
+      this.groupedItems = this.collection;
+    }
+  },
+  
+  render: function() {
+    if (this.collection) {
+      $(this.el).html(_.tpl('sheet', {
+        properties: this.properties,
+        propertyColors: this.propertyColors(),
+        group_keys: this.groupKeys(),
+        group_key: this.groupKey,
+        facets: this.facets,
+        collection: this.collection,
+        filtered_collection: this.filteredCollection
+      }));
+      
+      // Prepare properties to work with the visualization
+      var properties = [];
+      _.each(this.properties, function(p, index) {
+        if (!p.selected) return;
+        properties.push(p.property);
+      });
+      
+      if ((this.groupedItems && properties.length > 0)) {
+        this.visualization.update({
+          collection: this.groupedItems,
+          properties: properties,
+          propertyColors: this.propertyColors(),
+          id: this.groupKey
+        });
+      } else {
+        this.$('#canvas').html('<div class="info"><h2>Please choose some properties on the right tab.</h2></div>');
+      }
+    }
+    
+    this.delegateEvents();
+    return this;
+  }
+});
