e58fb2d1c3675c94d7748dacc39e02e11a5d33f9
Michael Aufreiter <ma@zive.at>
2011-04-28 21:51:04 +0200

    Display nice group headings.

--- public/javascripts/barchart.js
+++ public/javascripts/barchart.js
@@ -113,23 +113,24 @@ var Barchart = function(el, options) {
   
   function renderItems() {    
     // padding = 50;
-    itemOffset = barOffset*properties.length+60;
-    height = (c.items().length)*itemOffset+50;
+    // itemOffset = barOffset*properties.length+60;
+    // height = (c.items().length)*itemOffset+50;
     
     // Cleanup
     d3.select(el).selectAll('*').remove();
     
     // Init SVG
     var chart = d3.select(el)
-          .append("svg:svg")
-          .attr("class", "chart")
+          .append("div")
+          .attr("class", "chart barchart")
+          
           // .attr("width", width)
-          .attr("height", height)
-          .attr("fill", "#ccc")
-            .append("svg:g")
-            .attr("class", "plotarea")
-            .attr('width', 50)
-            .attr("transform", "translate(20, 0)")
+          // .attr("height", height)
+          // .attr("fill", "#ccc")
+            // .append("svg:g")
+            // .attr("class", "plotarea")
+            // .attr('width', 50)
+            // .attr("transform", "translate(20, 0)")
     
     // Items
     // --------------
@@ -146,13 +147,104 @@ var Barchart = function(el, options) {
       dataitems.set(key, item);
     });
     
-    var items = d3.select('g.plotarea')
-        .selectAll('g.item')
+    var items = d3.select('div.chart')
+        .selectAll('div.item')
         .data(dataitems)
-        .enter().append("svg:g")
+        .enter().append("div")
           .attr("class", "item")
-          .attr("transform", function(d, i) { return "translate(0, "+(i*itemOffset-0.5)+")"; })
+          // .attr("transform", function(d, i) { return "translate(0, "+(i*itemOffset-0.5)+")"; })
     
+    
+    // Append labels
+    // --------------
+
+    var prevGroupNames = _.map(id, function() { return ""; });
+    
+    var groupNames = items.selectAll('div.label').data(function(d) {
+              var groupNames = [];
+              
+              _.each(id, function(property) {
+                var part = d.get(property);
+                groupNames.push(part.values ? part.values().join(", ") : part);
+              });
+              
+              var res = [];
+              _.each(groupNames, function(name, index) {
+                if (name !== prevGroupNames[index]) {
+                  res.push(name);
+                } else {
+                  res.push("")
+                }
+              });
+              prevGroupNames = groupNames;
+              return res;
+            }).enter().append("div")
+              .attr("class", function(d, i) {
+                return "label level-"+(Math.min(prevGroupNames.length -i, 3));
+              })
+              .attr("style", function(d) { return d=="" ? "display: none;" : ""})
+              .html(function(d, i) { return d; })
+    
+    // Bars (Container)
+    // --------------
+
+    var barsContainers = items.append('svg:svg')
+      .attr('height', barOffset*properties.length)
+      .append('svg:g')
+        .attr('class', 'bars');
+        
+    var bars = barsContainers.selectAll('g.bar').data(function(d) {
+              return _.map(properties, function(property) {
+                return {
+                  value: d.get(property),
+                  property: c.properties().get(property)
+                }; // the value
+              });
+            }).enter().append("svg:g")
+              .attr("class", "bar")
+              .attr("transform", function(d,i) {
+                var xOffset = d.value < 0 ? scale(d.property, 0) - _.dist(scale(d.property, d.value), scale(d.property, 0)) : scale(d.property, 0);
+                xOffset = ~~xOffset;
+                return "translate("+xOffset+", "+(barOffset*i+0.5)+")";
+              });
+    
+    bars.on("mouseover", function(d) {
+      d3.select(this).selectAll('text')
+        .attr('fill', '#444');
+      d3.select(this).selectAll('rect')
+        .attr('opacity', 1.0);  
+    }).on("mouseout", function(d) {
+      d3.select(this).selectAll('text')
+        .attr('fill', '#999');
+      d3.select(this).selectAll('rect')
+        .attr('opacity', 0.8);
+    });
+
+
+    // Bars (Rectangle)
+    // --------------
+
+    bars.append('svg:rect')
+      .attr("height", barHeight)
+      .attr("width", function(d, i) {
+        return Math.max(~~_.dist(scale(d.property, d.value), scale(d.property, 0)), 2); 
+      })
+      .attr("fill", function(d, i) { return propertyColors(properties[i]); })
+      .attr("opacity", 0.8)
+      
+    bars.append('svg:text')
+      .text(function(d) {
+        var str = _.format(d.value, 2);
+        if (d.property.meta && d.property.meta.unit) str += " "+d.property.meta.unit;
+        return str;
+      })
+      .attr("x", function(d, i) {
+        return ~~_.dist(scale(d.property, d.value), scale(d.property, 0));
+      })
+      .attr("transform", "translate(5, 14)")
+      .attr("fill" , "#999")
+    
+        
     
     // Rulers
     // var rules = chart.selectAll("g.rule")
@@ -190,76 +282,11 @@ var Barchart = function(el, options) {
     //   .attr("y2", height)
     //   .attr("stroke", '#888')
     
-    // Bars (Container)
-    // --------------
-    
-    var bars = items.selectAll('g').data(function(d) {
-              return _.map(properties, function(property) {
-                return {
-                  value: d.get(property),
-                  property: c.properties().get(property)
-                }; // the value
-              });
-            }).enter().append("svg:g")
-              .attr("class", "bar")
-              .attr("transform", function(d,i) {
-                var xOffset = d.value < 0 ? scale(d.property, 0) - _.dist(scale(d.property, d.value), scale(d.property, 0)) : scale(d.property, 0);
-                xOffset = ~~xOffset;
-                return "translate("+xOffset+", "+(60+barOffset*i+0.5)+")";
-              });
 
-    bars.on("mouseover", function(d) {
-      d3.select(this).selectAll('text')
-        .attr('fill', '#444');
-      d3.select(this).selectAll('rect')
-        .attr('opacity', 1.0);  
-    }).on("mouseout", function(d) {
-      d3.select(this).selectAll('text')
-        .attr('fill', '#999');
-      d3.select(this).selectAll('rect')
-        .attr('opacity', 0.8);
-    });
-    
-    // Bars (Rectangle)
-    // --------------
-    
-    bars.append('svg:rect')
-      .attr("height", barHeight)
-      .attr("width", function(d, i) {
-        return Math.max(~~_.dist(scale(d.property, d.value), scale(d.property, 0)), 2); 
-      })
-      .attr("fill", function(d, i) { return propertyColors(properties[i]); })
-      .attr("opacity", 0.8)
+
+
+
       
-    bars.append('svg:text')
-      .text(function(d) {
-        var str = _.format(d.value, 2);
-        if (d.property.meta && d.property.meta.unit) str += " "+d.property.meta.unit;
-        return str;
-      })
-      .attr("x", function(d, i) {
-        return ~~_.dist(scale(d.property, d.value), scale(d.property, 0));
-      })
-      .attr("transform", "translate(5, 14)")
-      .attr("fill" , "#999")
-
-    // Append labels
-    // --------------
-    
-    items.append("svg:text")
-        .attr("class", "item-label")
-        .attr("transform", "translate("+ ~~scale(c.properties().get(properties[0]), 0) +", 40)")
-        .attr("fill", "#444")
-        .text(function(d, i) {
-          var groupNames = [];
-          
-          _.each(id, function(property) {
-            var part = d.get(property);
-            groupNames.push(part.values ? part.values().join(", ") : part);
-          });
-          
-          return groupNames.join(' / ');
-        });
   }
   
   function render() {
